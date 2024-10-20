e3f45b88a210ad252e2f1b0d78b03160f81b726e
Vikram <vikram.sh86@gmail.com>
2024-10-20 19:55:32 +0200

    Initial Commit

---
+++ Pantheon/PantheonApp.swift
@@ -0,0 +1,17 @@
+//
+//  PantheonApp.swift
+//  Pantheon
+//
+//  Created by Vikram on 20/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct PantheonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
